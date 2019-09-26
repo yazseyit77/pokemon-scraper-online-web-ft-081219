1e84dd463235320ff3c83ff3493018749d401996
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-26 13:56:53 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,19 @@
 class Pokemon
+  attr_accessor :name, :type, :db
+  attr_reader :id
+
+  def initialize(name)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save
+    db.execute( "INSERT INTO pokemon ( name, type ) VALUES ( ?, ? )", [name, type])
+  end
+
+  def self.find
+
+  end
 end
